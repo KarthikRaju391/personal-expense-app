43381317ee946d4f797a1375c254720654dc69a0
Karthik Raju <karthikraju391@gmail.com>
2021-04-12 17:43:56 +0530

    feature to add expense description

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
-//getting element
 const element = document.querySelector('#btn');
 const inputAmount = document.querySelector('#inputText');
+const inputDesc = document.querySelector("#inputDescription");
 let totalExpense = 0;
+
+//get the h1 element
+const headEl = document.querySelector('#headingElement');
+
+headEl.textContent = totalExpense;
 
 function addExpenseToTotal() {
 	//read the value from input
 	const enteredAmount = inputAmount.value;
-    const expense = parseInt(enteredAmount, 10);
+	const expense = parseInt(enteredAmount, 10);
 
-    console.log({expense});
+	console.log({ expense });
+    const expenseDesc = inputDesc.value;
+    console.log({expenseDesc});
 	//add it to totalExpense
-    totalExpense = totalExpense + expense;
+	totalExpense = totalExpense + expense;
 
-    console.log({totalExpense});
+	console.log({ totalExpense });
+
+	headEl.textContent = totalExpense;
 }
 
+//get the button element
 element.addEventListener('click', addExpenseToTotal, false);
