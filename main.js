b93ed5a51458d4f796d21c53696dbbf27fa3223b
Karthik Raju <karthikraju391@gmail.com>
2021-04-12 18:17:49 +0530

    templating

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const element = document.querySelector('#btn');
 const inputAmount = document.querySelector('#inputText');
-const inputDesc = document.querySelector("#inputDescription");
+const inputDesc = document.querySelector('#inputDescription');
+const expenseTable = document.querySelector("#expense-table");
 let totalExpense = 0;
 
 //get the h1 element
@@ -8,20 +9,40 @@ const headEl = document.querySelector('#headingElement');
 
 headEl.textContent = totalExpense;
 
+const allExpenses = [];
+
 function addExpenseToTotal() {
+	const expenseItem = {};
 	//read the value from input
 	const enteredAmount = inputAmount.value;
 	const expense = parseInt(enteredAmount, 10);
 
-	console.log({ expense });
-    const expenseDesc = inputDesc.value;
-    console.log({expenseDesc});
+	const expenseDesc = inputDesc.value;
 	//add it to totalExpense
 	totalExpense = totalExpense + expense;
 
-	console.log({ totalExpense });
+	expenseItem.Description = expenseDesc;
+	expenseItem.Amount = expense;
 
-	headEl.textContent = totalExpense;
+	allExpenses.push(expenseItem);
+
+    const someText = `Total: ${totalExpense}`
+
+	headEl.textContent = someText;
+
+    //show the table here
+    const data1 = allExpenses[0];
+    const data2 = allExpenses[1];
+
+    const data1Text = `Expense: ${data1.Amount} :: Description: ${data1.Description}`;
+    const data2Text = `Expense: ${data2.Amount} :: Description: ${data2.Description}`;   
+
+    const tableText = `
+    <div>${data1Text}</div>
+    <div>${data2Text}</div>
+    `
+
+    expenseTable.innerHTML = tableText;
 }
 
 //get the button element
